b32f67219710f0eaf7d124de4593dc7368d1dc01
Jannik Hauptvogel <jannik@ArchPC.fritz.box>
2017-11-26 00:17:04 +0100

    Switched from i3 to i3-gaps

--- de/i3/software.sh
+++ de/i3/software.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "i3" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop"
-        "termite" "dropbox-cli" "vlc" "qt4" "networkmanager" "network-manager-applet" "compton" "rofi" "xorg-xbacklight")
+pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop", "rofi"
+        "termite" "dropbox-cli" "vlc" "qt4" "networkmanager" "network-manager-applet" "compton" "xorg-xbacklight"
+        "i3-gaps", "i3blocks" "i3lock" "i3status")
 
 echo -e "[\033[1;34m*\e[0m] Installing i3 related packages"
 yaourt -S --noconfirm --needed ${pkg_i3[*]}
