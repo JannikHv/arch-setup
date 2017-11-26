b239b812f33eff261b92d9e710b4355d810ee8bd
Jannik Hauptvogel <jannik@ArchPad.fritz.box>
2017-11-26 14:15:54 +0100

    Several fixes and additions

--- de/i3/modules/datetime.sh
+++ de/i3/modules/datetime.sh
@@ -5,7 +5,7 @@ output=""
 function check_datetime() {
     local format="+%d.%m.%Y %H:%M:%S"
 
-    output+="  $(date --utc "${format}")"
+    output+="  $(date "${format}")"
 }
 
 check_datetime
--- de/i3/software.sh
+++ de/i3/software.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop", "rofi"
+pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop" "rofi"
         "termite" "dropbox-cli" "vlc" "qt4" "networkmanager" "network-manager-applet" "compton" "xorg-xbacklight"
-        "i3-gaps", "i3blocks" "i3lock" "i3status")
+        "i3-gaps" "i3blocks" "i3lock" "i3status" "htop" "lxappearance" "arc-gtk-theme")
 
 echo -e "[\033[1;34m*\e[0m] Installing i3 related packages"
 yaourt -S --noconfirm --needed ${pkg_i3[*]}
