70a737ee404648eb5c17bc7461ab33225bbeef5e
Jannik Hauptvogel <jannik@ArchPad.fritz.box>
2017-12-11 20:36:18 +0100

    Added colored i3blocks

--- de/i3/setup.sh
+++ de/i3/setup.sh
@@ -10,10 +10,10 @@ mkdir -p      ~/.config/i3/{wallpapers,modules,scripts}
 sudo mkdir -p /etc/xdg/termite/
 
 # Update files
-sudo cp -f "config/i3blocks.conf"  "/etc/i3blocks.conf"
-sudo cp -f "config/termite-config" "/etc/xdg/termite/config"
-cp -f      "config/i3-config"      "${HOME}/.config/i3/config"
-cp -f      "config/xinitrc"        "${HOME}/.xinitrc"
-cp -f      "config/Xresources"     "${HOME}/.Xresources"
-cp -rfT    "modules"               "${HOME}/.config/i3/modules"
-cp -rfT    "scripts"               "${HOME}/.config/i3/scripts"
+sudo cp -f "config/i3blocks-color.conf"  "/etc/i3blocks.conf"
+sudo cp -f "config/termite-config"       "/etc/xdg/termite/config"
+cp -f      "config/i3-config"            "${HOME}/.config/i3/config"
+cp -f      "config/xinitrc"              "${HOME}/.xinitrc"
+cp -f      "config/Xresources"           "${HOME}/.Xresources"
+cp -rfT    "modules"                     "${HOME}/.config/i3/modules"
+cp -rfT    "scripts"                     "${HOME}/.config/i3/scripts"
