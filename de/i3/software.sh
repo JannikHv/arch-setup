72710500bacb70513dcc23a57293ba63a8db34b7
Jannik Hauptvogel <JannikHv@gmail.com>
2017-12-17 20:56:17 +0100

    Added ranger for i3

--- de/i3/software.sh
+++ de/i3/software.sh
@@ -2,7 +2,8 @@
 
 pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop" "rofi"
         "termite" "dropbox-cli" "vlc" "qt4" "networkmanager" "network-manager-applet" "compton" "xorg-xbacklight"
-        "i3-gaps" "i3blocks" "i3lock" "i3status" "htop" "lxappearance" "arc-gtk-theme" "scrot" "imagemagick" "mupdf")
+        "i3-gaps" "i3blocks" "i3lock" "i3status" "htop" "lxappearance" "arc-gtk-theme" "scrot" "imagemagick" "mupdf"
+        "ranger")
 
 echo -e "[\033[1;34m*\e[0m] Installing i3 related packages"
 yaourt -S --noconfirm --needed ${pkg_i3[*]}
