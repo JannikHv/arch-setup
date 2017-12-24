0c0bcb5c1ca6fada35456d8a5f125dd3222154ac
Jannik Hauptvogel <JannikHv@gmail.com>
2017-12-24 17:48:24 +0100

    Added wallpaper

--- de/i3/setup.sh
+++ de/i3/setup.sh
@@ -10,10 +10,11 @@ mkdir -p      ~/.config/i3/{wallpapers,modules,scripts}
 sudo mkdir -p /etc/xdg/termite/
 
 # Update files
-sudo cp -f "config/i3blocks-bw.conf"  "/etc/i3blocks.conf"
+sudo cp -f "config/i3blocks-bw.conf"     "/etc/i3blocks.conf"
 sudo cp -f "config/termite-config"       "/etc/xdg/termite/config"
 cp -f      "config/i3-config"            "${HOME}/.config/i3/config"
 cp -f      "config/xinitrc"              "${HOME}/.xinitrc"
 cp -f      "config/Xresources"           "${HOME}/.Xresources"
 cp -rfT    "modules"                     "${HOME}/.config/i3/modules"
 cp -rfT    "scripts"                     "${HOME}/.config/i3/scripts"
+cp -f      "wallpapers/Countries.jpg"    "${HOME}/.config/i3/wallpapers/Default.jpg"
