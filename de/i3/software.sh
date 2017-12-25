5bc2e66d442fb3f29d5752da96c83e2d29fec42e
Jannik Hauptvogel <JannikHv@gmail.com>
2017-12-25 19:25:20 +0100

    Removed burn utils, added soundconverter

--- de/i3/software.sh
+++ de/i3/software.sh
@@ -3,8 +3,10 @@
 pkg_i3=("acpi" "playerctl" "alsa-utils" "feh" "pulseaudio" "pulseaudio-alsa" "rhythmbox" "xorg-xprop" "rofi"
         "termite" "dropbox-cli" "vlc" "qt4" "networkmanager" "network-manager-applet" "compton" "xorg-xbacklight"
         "i3-gaps" "i3blocks" "i3lock" "i3status" "htop" "lxappearance" "arc-gtk-theme" "scrot" "imagemagick" "mupdf"
-        "ranger" "mpv" "xbanish" "nodejs" "electron"
-        "brasero" "libburn" "libisofs" "dvdauthor" "vcdimager")
+        "ranger" "mpv" "xbanish" "nodejs" "electron" "soundconverter")
+
+# Burning utilities:
+# brasero libburn libisofs dvdauthor vcdimager
 
 echo -e "[\033[1;34m*\e[0m] Installing i3 related packages"
 yaourt -S --noconfirm --needed ${pkg_i3[*]}
