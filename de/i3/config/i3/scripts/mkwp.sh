265864487337db846611da2af1108087def2c640
Jannik Hauptvogel <hauptvogel@amicaldo.de>
2018-07-30 17:40:27 +0200

    Improved syntax and functionality

--- de/i3/config/i3/scripts/mkwp.sh
+++ de/i3/config/i3/scripts/mkwp.sh
@@ -9,7 +9,32 @@ if [ ! $1 ]; then
     exit 1
 fi
 
-rm -rf ~/.config/i3/wallpapers/*
+refresh_wp() {
+    feh --bg-scale ~/.config/i3/wallpapers/Default*
+}
 
-cp "${1}" ~/.config/i3/wallpapers/Default
-feh --bg-scale ~/.config/i3/wallpapers/Default*
+clear_wp() {
+    rm -rf ~/.config/i3/wallpapers/*
+}
+
+assign_wp() {
+    local wpFile="${1}"
+
+    clear_wp
+    cp "${wpFile}" ~/.config/i3/wallpapers/Default
+    refresh_wp
+}
+
+for arg in $@; do
+    case $1 in
+        -r | --refresh)
+            refresh_wp
+            shift
+            ;;
+        *)
+            assign_wp ${1}
+            shift
+            ;;
+    esac
+done
+
